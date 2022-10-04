8b1b5e35c76b5765deec07a770d18d69b01e59f7
Adeeb waiz <waizadeeb@gmail.com>
2022-10-04 18:50:10 +0400

    first one

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { render } from '@testing-library/react';
+import React, { useState } from "react";
+import DataFetching from './DataFetching';
+
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
